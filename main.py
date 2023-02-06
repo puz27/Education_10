0771e509eb7eda12dddf9eabd742127f65b4586e
anv <anv@woori.ru>
2023-02-06 17:51:06 +0300

    add Flask

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = "ABOUT PAGE"
+    return render_template("index.html")
+
+
 if __name__ == '__main__':
     print('PyCharm')
+    app.run(debug=True)
+
+
+
--- utils.py
+++ utils.py
@@ -2,39 +2,59 @@ import json
 import os
 
 
-def load_candidates(file_name: str) ->list :
+def load_candidates(file_name: str) -> list:
     """
     load data from local file
     :param file_name: name of import file
     :return: list of dictionaries
     """
-    path_file = os.path.join(os.getcwd(), file_name)
+    path_file = os.path.join(os.getcwd(), "data", file_name)
     file = open(path_file, 'r', encoding='utf-8')
     data = json.load(file)
     file.close()
     return data
 
-data_of_candidats = load_candidates("candidates.json")
-#print(data_of_candidats)
 
-def get_all(data_of_candidats):
-    for candidate in data_of_candidats:
-        print(candidate["name"])
+def get_all(candidates: list) -> list:
+    """
+    load all candidates
+    :param candidates: list with candidates
+    :return: list
+    """
+    needed_candidates = []
+    for candidate in candidates:
+        needed_candidates.append(candidate["name"])
+    return needed_candidates
 
-def get_by_pk(pk, data_of_candidats):
-    for candidate in data_of_candidats:
+
+def get_by_pk(pk: int, candidates: list) -> str:
+    """
+    :param pk: number that need to find
+    :param candidates: list with candidates
+    :return: information about candidate
+    """
+    for candidate in candidates:
         if candidate.get("pk") == pk:
-            print(candidate)
-    print("Такого кандидата нет")
+            return candidate
+    return "Такого кандидата нет"
 
-def get_by_skill(skill_name, data_of_candidats):
-    skill_needed = [skill_name]
-    for candidate in data_of_candidats:
-        skills_known = (set(candidate["skills"].split(", ")))
-        #print(skills_known.intersection(set(skill_needed)))
-        #print((set(skill_needed)).issubset(skills_known))
+
+def get_by_skill(skill_name: str, candidates: list) -> list:
+    """
+    :param skill_name: skill that need to find
+    :param candidates: list with candidates
+    :return: list on users with needed skills
+    """
+    candidates_with_needed_skills = []
+    skill_needed = [skill_name.lower()]
+    for candidate in candidates:
+        skills_known = (set(candidate["skills"].lower().split(", ")))
         if set(skill_needed).issubset(skills_known) is True:
-            print(candidate)
+            candidates_with_needed_skills.append(candidate)
+    return candidates_with_needed_skills
 
 
-print(get_by_skill('python', data_of_candidats))
+data_of_candidats = load_candidates("candidates.json")
+print(get_all(data_of_candidats))
+print(get_by_pk(2, data_of_candidats))
+print(get_by_skill("Python", data_of_candidats))
